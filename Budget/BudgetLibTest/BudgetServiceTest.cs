78c3f2879c72ff7e27a9ea86031081ac735b4d9f
Kevin Liao <kevin.liao@mail.aircast.biz>
2019-07-27 17:26:20 +0800

    2 months

--- Budget/BudgetLibTest/BudgetServiceTest.cs
+++ Budget/BudgetLibTest/BudgetServiceTest.cs
@@ -89,18 +89,18 @@ namespace BudgetLibTest
             BudgetShouldBe(new DateTime(2019, 7, 1), new DateTime(2019, 7, 5), 500);
         }
 
-        //[Test]
-        //public void TwoMonth_Budget_300()
-        //{
-        //    _stubBudgetRepo.GetAll()
-        //        .Returns(new List<Budget>
-        //        {
-        //            new Budget {YearMonth = "201907", Amount = 3100},
-        //            new Budget {YearMonth = "201908", Amount = 3100}
-        //    });
+        [Test]
+        public void TwoMonth_Budget_300()
+        {
+            _stubBudgetRepo.GetAll()
+                .Returns(new List<Budget>
+                {
+                    new Budget {YearMonth = "201907", Amount = 3100},
+                    new Budget {YearMonth = "201908", Amount = 3100}
+            });
 
-        //    BudgetShouldBe(new DateTime(2019, 7, 1), new DateTime(2019, 8, 2), 3300);
-        //}
+            BudgetShouldBe(new DateTime(2019, 7, 1), new DateTime(2019, 8, 2), 3300);
+        }
 
         private void BudgetShouldBe(DateTime startDate, DateTime endDate, double expected)
         {
