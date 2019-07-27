11785e7381d06a17cba8530849e07417e053296e
Kevin Liao <kevin.liao@mail.aircast.biz>
2019-07-27 15:50:50 +0800

    first commit

---
+++ Budget/BudgetLibTest/BudgetService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BudgetLibTest
+{
+    public class BudgetService
+    {
+        private readonly IBudgetRepo _budgetRepo;
+
+        public BudgetService(IBudgetRepo budgetRepo)
+        {
+            _budgetRepo = budgetRepo;
+        }
+
+        public double Query(DateTime startDate, DateTime endDate)
+        {
+            if (IsInvalidDateRange(startDate, endDate))
+            {
+                return 0;
+            }
+
+            var budgets = _budgetRepo.GetAll() ?? new List<Budget>();
+
+            return budgets.FirstOrDefault()?.Amount ?? 0;
+        }
+
+        private static bool IsInvalidDateRange(DateTime startDate, DateTime endDate)
+        {
+            return endDate < startDate;
+        }
+    }
+}
---
+++ Budget/BudgetLibTest/BudgetServiceTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BudgetLibTest
+{
+    [TestFixture]
+    public class BudgetServiceTest
+    {
+        private BudgetService _budgetService;
+        private IBudgetRepo _stubBudgetRepo;
+
+        [SetUp]
+        public void Setup()
+        {
+            _stubBudgetRepo = Substitute.For<IBudgetRepo>();
+            _budgetService = new BudgetService(_stubBudgetRepo);
+        }
+
+        [Test]
+        public void Invalid_DateRange()
+        {
+            BudgetShouldBe(new DateTime(2019, 7, 29), new DateTime(2019, 7, 28), 0);
+        }
+
+        [Test]
+        public void Valid_DateRange()
+        {
+            BudgetShouldBe(new DateTime(2019, 7, 27), new DateTime(2019, 7, 28), 0);
+        }
+
+        [Test]
+        public void SingleWholeMonth_HasBudget_()
+        {
+            _stubBudgetRepo.GetAll()
+                .Returns(new List<Budget>
+                {
+                    new Budget {YearMonth = "201907", Amount = 3100}
+                });
+
+            BudgetShouldBe(new DateTime(2019, 7, 1), new DateTime(2019, 7, 31), 3100);
+        }
+
+        private void BudgetShouldBe(DateTime startDate, DateTime endDate, double expected)
+        {
+            var actual = _budgetService.Query(startDate, endDate);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public interface IBudgetRepo
+    {
+        List<Budget> GetAll();
+    }
+
+    public class Budget
+    {
+        public string YearMonth { get; set; }
+        public int Amount { get; set; }
+    }
+}
