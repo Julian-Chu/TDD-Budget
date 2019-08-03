7d76a299814af202a523e79f47a1f4ba6492d786
Julian Chu <yulang.chu@gmail.com>
2019-08-04 01:28:35 +0800

    move GetDaysInMonth to BudgetService\i

--- Budget/BudgetLibTest/BudgetService.cs
+++ Budget/BudgetLibTest/BudgetService.cs
@@ -20,7 +20,7 @@ namespace BudgetLibTest
                 return 0;
             }
 
-            Dictionary<string, int> days = Help.GetDaysInMonth(startDate, endDate);
+            Dictionary<string, int> days = GetDaysInMonth(startDate, endDate);
 
             var budgets = _budgetRepo.GetAll() ?? new List<Budget>();
 
@@ -43,6 +43,36 @@ namespace BudgetLibTest
         private static bool IsInvalidDateRange(DateTime startDate, DateTime endDate)
         {
             return endDate < startDate;
+        }
+
+        public  Dictionary<string, int> GetDaysInMonth(DateTime startDate, DateTime endDate)
+        {
+            Dictionary<string, int> lookup = new Dictionary<string, int>();
+            if (startDate.Year == endDate.Year && startDate.Month == endDate.Month)
+            {
+                lookup.Add(startDate.ToString("yyyyMM"), (endDate - startDate).Days + 1);
+                return lookup;
+            }
+
+            var start = new DateTime(startDate.Year, startDate.Month, 1);
+            while (start <= endDate)
+            {
+                var days = DateTime.DaysInMonth(start.Year, start.Month);
+                if (start.Month == startDate.Month && start.Year == startDate.Year)
+                {
+                    days = days - startDate.Day + 1;
+                }
+                if (start.Month == endDate.Month && start.Year == endDate.Year)
+                {
+                    days = endDate.Day;
+                }
+
+                lookup[start.ToString("yyyyMM")] = days;
+
+                start = start.AddMonths(1);
+            }
+
+            return lookup;
         }
     }
 
